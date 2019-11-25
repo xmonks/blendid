6f4d6745b8c6682234b3494adfeb94dcbb5b39af
Aleš Roubíček <ales@roubicek.name>
2019-11-25 09:23:31 +0100

    Added locales support

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -19,6 +19,23 @@ const getEnabledTasks = require("./lib/getEnabledTasks");
 global.PATH_CONFIG = require("./lib/get-path-config");
 global.TASK_CONFIG = require("./lib/get-task-config");
 
+const areIntlLocalesSupported = require("intl-locales-supported").default;
+const localesMyAppSupports = TASK_CONFIG.locales || ["cs", "en"];
+
+if (global.Intl) {
+  // Determine if the built-in `Intl` has the locale data we need.
+  if (!areIntlLocalesSupported(localesMyAppSupports)) {
+    // `Intl` exists, but it doesn't have the data we need, so load the
+    // polyfill and patch the constructors we need with the polyfill's.
+    const IntlPolyfill = require("intl");
+    Intl.NumberFormat = IntlPolyfill.NumberFormat;
+    Intl.DateTimeFormat = IntlPolyfill.DateTimeFormat;
+  }
+} else {
+  // No `Intl`, so use and load the polyfill.
+  global.Intl = require("intl");
+}
+
 require("./tasks/browserSync");
 require("./tasks/clean");
 require("./tasks/fonts");
--- gulpfile.js/tasks/rev/rev-update-references.js
+++ gulpfile.js/tasks/rev/rev-update-references.js
@@ -4,7 +4,10 @@ const projectPath = require("../../lib/projectPath");
 
 // 2) Update asset references with reved filenames in compiled css + js
 gulp.task("rev-update-references", function() {
-  const manifest = gulp.src(projectPath(PATH_CONFIG.dest, "rev-manifest.json"));
+  const manifest = gulp.src(
+    projectPath(PATH_CONFIG.dest, "rev-manifest.json"),
+    { allowEmpty: true }
+  );
 
   return gulp
     .src(projectPath(PATH_CONFIG.dest, "**/**.{css,js}"))
