5380754469fa119bdaf5910dbba70a4be5889626
Aleš Roubíček <ales@roubicek.name>
2020-02-02 10:46:36 +0100

    Cloudinary upload polishing

--- gulpfile.js/tasks/cloudinary.js
+++ gulpfile.js/tasks/cloudinary.js
@@ -6,36 +6,34 @@ const changedInPlace = require("gulp-changed-in-place");
 const path = require("path");
 const projectPath = require("../lib/projectPath");
 
+const paths = {
+  src: projectPath(PATH_CONFIG.src, PATH_CONFIG.cloudinary.src),
+  dest: projectPath(PATH_CONFIG.src, PATH_CONFIG.data.src)
+};
+
 const cloudinaryTask = () =>
-  src(
-    projectPath(
-      PATH_CONFIG.src,
-      PATH_CONFIG.cloudinary.src,
-      `**/*.{${TASK_CONFIG.cloudinary.extensions}}`
-    )
-  )
+  src(path.join(paths.src, "**", `*.{${TASK_CONFIG.cloudinary.extensions}}`))
     .pipe(changedInPlace())
     .pipe(
       cloudinaryUpload({
         folderResolver(filePath) {
           const relativePath = path.relative(
-            projectPath(PATH_CONFIG.src, PATH_CONFIG.cloudinary.src),
+            paths.src,
             path.resolve(__dirname, path.dirname(filePath))
           );
           return path.join(PATH_CONFIG.cloudinary.dest, relativePath);
+        },
+        keyResolver(filePath) {
+          return path.relative(paths.src, path.resolve(__dirname, filePath));
         }
       })
     )
     .pipe(
       cloudinaryUpload.manifest({
-        path: projectPath(
-          PATH_CONFIG.src,
-          PATH_CONFIG.data.src,
-          TASK_CONFIG.cloudinary.manifest
-        )
+        path: path.join(paths.dest, TASK_CONFIG.cloudinary.manifest)
       })
     )
-    .pipe(dest(projectPath(PATH_CONFIG.src, PATH_CONFIG.data.src)));
+    .pipe(dest(paths.dest));
 
 task("cloudinary", cloudinaryTask);
 module.exports = cloudinaryTask;
