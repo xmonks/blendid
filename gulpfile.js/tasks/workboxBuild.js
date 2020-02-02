d8c893488bfb2ba967b01bab9788b9227a45ca11
Aleš Roubíček <ales@roubicek.name>
2020-02-02 08:44:24 +0100

    prettier

--- gulpfile.js/tasks/workboxBuild.js
+++ gulpfile.js/tasks/workboxBuild.js
@@ -2,34 +2,36 @@ if (!TASK_CONFIG.workboxBuild) return;
 
 const gulp = require("gulp");
 const projectPath = require("../lib/projectPath");
-const { generateSW, injectManifest } = require('workbox-build');
+const { generateSW, injectManifest } = require("workbox-build");
 
 const transformConfigPaths = ({ globDirectory, swDest, swSrc, ...config }) => {
-
   if (globDirectory) {
-    config.globDirectory = projectPath(globDirectory)
+    config.globDirectory = projectPath(globDirectory);
   }
 
   if (swDest) {
-    config.swDest = projectPath(swDest)
+    config.swDest = projectPath(swDest);
   }
 
   if (swSrc) {
-    config.swSrc = projectPath(swSrc)
+    config.swSrc = projectPath(swSrc);
   }
 
-
   return config;
-}
+};
 
 const workboxBuildTask = function() {
   const config = transformConfigPaths(TASK_CONFIG.workboxBuild);
-  const useInjectManifest = typeof config.swSrc === 'string';
+  const useInjectManifest = typeof config.swSrc === "string";
 
   if (useInjectManifest) {
-    return injectManifest(config).catch(error => { throw error; });
+    return injectManifest(config).catch(error => {
+      throw error;
+    });
   } else {
-    return generateSW(config).catch(error => { throw error; });
+    return generateSW(config).catch(error => {
+      throw error;
+    });
   }
 };
 
