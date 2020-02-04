0a0f96c217fa846e81e48d7f1589ca396c9d4fc9
Aleš Roubíček <ales@roubicek.name>
2020-02-04 13:49:09 +0100

    Filter already uploaded images

--- gulpfile.js/tasks/cloudinary.js
+++ gulpfile.js/tasks/cloudinary.js
@@ -1,26 +1,54 @@
 if (!TASK_CONFIG.cloudinary) return;
 
+const fs = require("fs");
 const { task, src, dest, lastRun } = require("gulp");
 const cloudinaryUpload = require("gulp-cloudinary-upload");
+const changed = require("gulp-changed");
 const path = require("path");
 const projectPath = require("../lib/projectPath");
 
 const paths = {
   src: projectPath(PATH_CONFIG.src, PATH_CONFIG.cloudinary.src),
-  dest: projectPath(PATH_CONFIG.src, PATH_CONFIG.data.src)
+  dest: projectPath(PATH_CONFIG.src, PATH_CONFIG.data.src),
+  manifest: projectPath(
+    PATH_CONFIG.src,
+    PATH_CONFIG.data.src,
+    TASK_CONFIG.cloudinary.manifest
+  )
 };
+
+async function readManifest(path) {
+  return fs.promises
+    .readFile(path, "utf-8")
+    .then(x => JSON.parse(x))
+    .catch(() => null);
+}
+
+function getRelativePath(filePath) {
+  return path.relative(
+    paths.src,
+    path.resolve(__dirname, path.dirname(filePath))
+  );
+}
 
 const cloudinaryTask = () =>
   src(path.join(paths.src, "**", `*.{${TASK_CONFIG.cloudinary.extensions}}`), {
     since: lastRun(cloudinaryTask)
   })
     .pipe(
+      changed(paths.dest, {
+        async hasChanged(stream, sourceFile, targetPath) {
+          const manifest = await readManifest(paths.manifest);
+          if (!(manifest && manifest[getRelativePath(sourceFile.path)])) {
+            stream.push(sourceFile);
+          }
+        }
+      })
+    )
+    .pipe(
       cloudinaryUpload({
         folderResolver(filePath) {
-          const relativePath = path.relative(
-            paths.src,
-            path.resolve(__dirname, path.dirname(filePath))
-          );
+          const relativePath = getRelativePath(filePath);
           return path.join(PATH_CONFIG.cloudinary.dest, relativePath);
         },
         keyResolver(filePath) {
@@ -30,7 +58,7 @@ const cloudinaryTask = () =>
     )
     .pipe(
       cloudinaryUpload.manifest({
-        path: path.join(paths.dest, TASK_CONFIG.cloudinary.manifest),
+        path: paths.manifest,
         merge: true
       })
     )
