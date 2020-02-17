922824b9928d7f65e242143b9e804f8ab2885c99
Aleš Roubíček <ales@roubicek.name>
2020-02-17 15:28:16 +0100

    Replace revs in relative paths ganerated by Rollup

--- gulpfile.js/tasks/rev/update-js.js
+++ gulpfile.js/tasks/rev/update-js.js
@@ -4,9 +4,22 @@ const { task, src, dest } = require("gulp");
 const revReplace = require("gulp-rev-replace");
 const projectPath = require("../../lib/projectPath");
 
+const paths = {
+  src: projectPath(PATH_CONFIG.dest, PATH_CONFIG.javascripts.dest, "**/*.js"),
+  dest: projectPath(PATH_CONFIG.dest, PATH_CONFIG.javascripts.dest)
+};
+
+const relativePath = s => s.replace(PATH_CONFIG.javascripts.dest, ".");
+
 task("update-js", function() {
   const manifest = src(projectPath(PATH_CONFIG.dest, "rev-manifest.json"));
-  return src(projectPath(PATH_CONFIG.dest, PATH_CONFIG.javascripts.dest, "**/*.js"))
-    .pipe(revReplace({ manifest: manifest }))
-    .pipe(dest(projectPath(PATH_CONFIG.dest, PATH_CONFIG.javascripts.dest)));
+  return src(paths.src)
+    .pipe(
+      revReplace({
+        manifest,
+        modifyUnreved: relativePath,
+        modifyReved: relativePath
+      })
+    )
+    .pipe(dest(paths.dest));
 });
