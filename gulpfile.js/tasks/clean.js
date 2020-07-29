ec7472ae6d226fcfa3e80d78b6d4d2660c7a14f6
Aleš Roubíček <ales@roubicek.name>
2020-07-29 14:52:49 +0200

    Return promise from clean task to prevent race-conditions

--- gulpfile.js/tasks/clean.js
+++ gulpfile.js/tasks/clean.js
@@ -2,14 +2,12 @@ const gulp = require("gulp");
 const del = require("del");
 const projectPath = require("../lib/projectPath");
 
-const cleanTask = function(cb) {
+const cleanTask = function() {
   const patterns =
     TASK_CONFIG.clean && TASK_CONFIG.clean.patterns
       ? TASK_CONFIG.clean.patterns
       : projectPath(PATH_CONFIG.dest);
-  console.log("clean", { patterns });
-  del(patterns, { force: true });
-  cb();
+  return del(patterns, { force: true });
 };
 
 gulp.task("clean", cleanTask);
