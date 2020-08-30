39324a22fa91e3ff732a1d546e4eb14b69c46ea4
Aleš Roubíček <ales@roubicek.name>
2020-08-30 17:46:02 +0200

    try to fix broken browsersync watch

--- gulpfile.js/tasks/browserSync.js
+++ gulpfile.js/tasks/browserSync.js
@@ -1,14 +1,14 @@
 if (global.production) return;
 
 const browserSync = require("browser-sync").create("blendid");
-const gulp = require("gulp");
+const { watch, task } = require("gulp");
 const projectPath = require("../lib/projectPath");
 
-const browserSyncTask = function(cb) {
+const browserSyncTask = function (cb) {
   const config = TASK_CONFIG.browserSync;
   if (typeof config.proxy === "string") {
     config.proxy = {
-      target: config.proxy
+      target: config.proxy,
     };
   }
 
@@ -19,7 +19,7 @@ const browserSyncTask = function(cb) {
 
   // Resolve files from project
   if (config.files) {
-    config.files = config.files.map(glob => projectPath(glob));
+    config.files = config.files.map((glob) => projectPath(glob));
   }
 
   const server = config.proxy || config.server;
@@ -27,10 +27,10 @@ const browserSyncTask = function(cb) {
 
   browserSync.init(config);
 
-  const output = projectPath(PATH_CONFIG.dest, "**/*");
-  browserSync.watch(output).on("change", browserSync.reload);
+  const output = projectPath(PATH_CONFIG.dest, "**/*.{html,js,css}");
+  watch(output, browserSync.reload);
   cb();
 };
 
-gulp.task("browserSync", browserSyncTask);
+task("browserSync", browserSyncTask);
 module.exports = browserSyncTask;
