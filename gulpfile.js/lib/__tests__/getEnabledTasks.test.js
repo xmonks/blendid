a8eb2db179e2ee09b4af825c371e9c2cc2363c71
Aleš Roubíček <ales@roubicek.name>
2020-11-03 06:45:14 +0100

    Fixed codeTasks tests

--- gulpfile.js/lib/__tests__/getEnabledTasks.test.js
+++ gulpfile.js/lib/__tests__/getEnabledTasks.test.js
@@ -3,42 +3,42 @@ const forEach = require("lodash/forEach");
 const getEnabledTasks = require("../getEnabledTasks");
 const keys = require("lodash/keys");
 
-describe("getEnabledTasks", function() {
-  describe("when env == development", function() {
-    beforeEach(function() {
+describe("getEnabledTasks", function () {
+  describe("when env == development", function () {
+    beforeEach(function () {
       ENV = "development";
     });
 
-    describe("#assetTasks", function() {
-      beforeEach(function() {
+    describe("#assetTasks", function () {
+      beforeEach(function () {
         TASK_CONFIG = {
           cloudinary: true,
           fonts: true,
           iconFont: true,
           images: true,
-          svgSprite: true
+          svgSprite: true,
         };
       });
 
-      it("returns all tasks when none disabled", function() {
+      it("returns all tasks when none disabled", function () {
         const tasks = getEnabledTasks(ENV);
         assert.deepEqual(tasks.assetTasks, [
           "cloudinary",
           "fonts",
           "iconFont",
-          "images"
+          "images",
         ]);
       });
 
-      it("returns only enabled task when some disabled", function() {
+      it("returns only enabled task when some disabled", function () {
         TASK_CONFIG["iconFont"] = false;
 
         const tasks = getEnabledTasks(ENV);
         assert.deepEqual(tasks.assetTasks, ["cloudinary", "fonts", "images"]);
       });
 
-      it("returns false when all disabled", function() {
-        forEach(keys(TASK_CONFIG), function(key) {
+      it("returns false when all disabled", function () {
+        forEach(keys(TASK_CONFIG), function (key) {
           TASK_CONFIG[key] = false;
         });
 
@@ -47,33 +47,29 @@ describe("getEnabledTasks", function() {
       });
     });
 
-    describe("#codeTasks", function() {
-      beforeEach(function() {
+    describe("#codeTasks", function () {
+      beforeEach(function () {
         TASK_CONFIG = {
           html: true,
           stylesheets: true,
-          javascripts: true
+          javascripts: true,
         };
       });
 
-      it("returns all except javascripts when none disabled", function() {
+      it("returns all except javascripts when none disabled", function () {
         const tasks = getEnabledTasks(ENV);
-        assert.deepEqual(tasks.codeTasks, [
-          "html",
-          "stylesheets",
-          "javascripts"
-        ]);
+        assert.deepEqual(tasks.codeTasks, ["stylesheets", "javascripts"]);
       });
 
-      it("returns only enabled except javascripts task when some disabled", function() {
+      it("returns only enabled except javascripts task when some disabled", function () {
         TASK_CONFIG["stylesheets"] = false;
 
         const tasks = getEnabledTasks(ENV);
-        assert.deepEqual(tasks.codeTasks, ["html", "javascripts"]);
+        assert.deepEqual(tasks.codeTasks, ["javascripts"]);
       });
 
-      it("returns false when all disabled", function() {
-        forEach(keys(TASK_CONFIG), function(key) {
+      it("returns false when all disabled", function () {
+        forEach(keys(TASK_CONFIG), function (key) {
           TASK_CONFIG[key] = false;
         });
 
@@ -83,40 +79,40 @@ describe("getEnabledTasks", function() {
     });
   });
 
-  describe("when env == production", function() {
-    beforeEach(function() {
+  describe("when env == production", function () {
+    beforeEach(function () {
       ENV = "production";
     });
 
-    describe("#assetTasks", function() {
-      beforeEach(function() {
+    describe("#assetTasks", function () {
+      beforeEach(function () {
         TASK_CONFIG = {
           cloudinary: true,
           fonts: true,
           iconFont: true,
-          images: true
+          images: true,
         };
       });
 
-      it("returns all tasks when none disabled", function() {
+      it("returns all tasks when none disabled", function () {
         const tasks = getEnabledTasks(ENV);
         assert.deepEqual(tasks.assetTasks, [
           "cloudinary",
           "fonts",
           "iconFont",
-          "images"
+          "images",
         ]);
       });
 
-      it("returns only enabled task when some disabled", function() {
+      it("returns only enabled task when some disabled", function () {
         TASK_CONFIG["iconFont"] = false;
 
         const tasks = getEnabledTasks(ENV);
         assert.deepEqual(tasks.assetTasks, ["cloudinary", "fonts", "images"]);
       });
 
-      it("returns false when all disabled", function() {
-        forEach(keys(TASK_CONFIG), function(key) {
+      it("returns false when all disabled", function () {
+        forEach(keys(TASK_CONFIG), function (key) {
           TASK_CONFIG[key] = false;
         });
 
@@ -125,40 +121,36 @@ describe("getEnabledTasks", function() {
       });
     });
 
-    describe("#codeTasks", function() {
-      beforeEach(function() {
+    describe("#codeTasks", function () {
+      beforeEach(function () {
         TASK_CONFIG = {
           html: true,
           stylesheets: true,
-          javascripts: true
+          javascripts: true,
         };
       });
 
-      it("returns all and convert javascripts task when none disabled", function() {
+      it("returns all and convert javascripts task when none disabled", function () {
         const tasks = getEnabledTasks(ENV);
-        assert.deepEqual(tasks.codeTasks, [
-          "html",
-          "stylesheets",
-          "javascripts"
-        ]);
+        assert.deepEqual(tasks.codeTasks, ["stylesheets", "javascripts"]);
       });
 
-      it("returns only enabled and convert javascripts task when some disabled", function() {
+      it("returns only enabled and convert javascripts task when some disabled", function () {
         TASK_CONFIG["stylesheets"] = false;
 
         const tasks = getEnabledTasks(ENV);
-        assert.deepEqual(tasks.codeTasks, ["html", "javascripts"]);
+        assert.deepEqual(tasks.codeTasks, ["javascripts"]);
       });
 
-      it("still correctly disable javascripts task when disabled", function() {
+      it("still correctly disable javascripts task when disabled", function () {
         TASK_CONFIG["javascripts"] = false;
 
         const tasks = getEnabledTasks(ENV);
-        assert.deepEqual(tasks.codeTasks, ["html", "stylesheets"]);
+        assert.deepEqual(tasks.codeTasks, ["stylesheets"]);
       });
 
-      it("returns false when all disabled", function() {
-        forEach(keys(TASK_CONFIG), function(key) {
+      it("returns false when all disabled", function () {
+        forEach(keys(TASK_CONFIG), function (key) {
           TASK_CONFIG[key] = false;
         });
 
