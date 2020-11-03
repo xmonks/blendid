6eaa84c7ab6b41612ee8f136a8351ca9162a4588
Aleš Roubíček <ales@roubicek.name>
2020-11-03 06:07:17 +0100

    Fix sizereport never completes in pipelines

--- gulpfile.js/tasks/sizereport.js
+++ gulpfile.js/tasks/sizereport.js
@@ -1,14 +1,9 @@
-const stream = require("stream");
-const util = require("util");
 const { src, task } = require("gulp");
 const sizereport = require("gulp-sizereport");
 const projectPath = require("../lib/projectPath");
 
-const pipeline = util.promisify(stream.pipeline);
-
-task("size-report", function() {
-  return pipeline(
-    src([projectPath(PATH_CONFIG.dest, "**/*"), "*!rev-manifest.json"]),
+task("size-report", () =>
+  src([projectPath(PATH_CONFIG.dest, "**/*"), "*!rev-manifest.json"]).pipe(
     sizereport({ gzip: true })
-  );
-});
+  )
+);
