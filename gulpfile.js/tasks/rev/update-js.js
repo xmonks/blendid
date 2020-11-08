ae46df41ce1053d142614e91ae430d90a8ce32ae
Aleš Roubíček <ales@roubicek.name>
2020-11-08 15:14:24 +0100

    Initial support for esbuilld

--- gulpfile.js/tasks/rev/update-js.js
+++ gulpfile.js/tasks/rev/update-js.js
@@ -1,24 +1,26 @@
-if (!TASK_CONFIG.javascripts) return false;
+if (!(TASK_CONFIG.javascripts || TASK_CONFIG.esbuild)) return false;
 
 const { task, src, dest } = require("gulp");
 const revReplace = require("gulp-rev-replace");
 const projectPath = require("../../lib/projectPath");
 
+const destDir = (PATH_CONFIG.javascripts || PATH_CONFIG.esbuild).dest;
+
 const paths = {
-  src: projectPath(PATH_CONFIG.dest, PATH_CONFIG.javascripts.dest, "**/*.js"),
-  dest: projectPath(PATH_CONFIG.dest, PATH_CONFIG.javascripts.dest)
+  src: projectPath(PATH_CONFIG.dest, destDir, "**/*.js"),
+  dest: projectPath(PATH_CONFIG.dest, destDir),
 };
 
-const relativePath = s => s.replace(PATH_CONFIG.javascripts.dest, ".");
+const relativePath = (s) => s.replace(destDir, ".");
 
-task("update-js", function() {
+task("update-js", function () {
   const manifest = src(projectPath(PATH_CONFIG.dest, "rev-manifest.json"));
   return src(paths.src)
     .pipe(
       revReplace({
         manifest,
         modifyUnreved: relativePath,
-        modifyReved: relativePath
+        modifyReved: relativePath,
       })
     )
     .pipe(dest(paths.dest));
