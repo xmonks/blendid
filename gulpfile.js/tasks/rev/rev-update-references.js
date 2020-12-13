efd0530fd8cfa0594829f6dbe8228725d543f8ba
Aleš Roubíček <ales@roubicek.name>
2020-12-13 03:00:46 +0100

    rev sourcemaps

--- gulpfile.js/tasks/rev/rev-update-references.js
+++ gulpfile.js/tasks/rev/rev-update-references.js
@@ -3,12 +3,12 @@ const revReplace = require("gulp-rev-replace");
 const projectPath = require("../../lib/projectPath");
 
 // 2) Update asset references with reved filenames in compiled css + js
-task("rev-update-references", function() {
+task("rev-update-references", function () {
   const manifest = src(projectPath(PATH_CONFIG.dest, "rev-manifest.json"), {
-    allowEmpty: true
+    allowEmpty: true,
   });
 
-  return src(projectPath(PATH_CONFIG.dest, "**/**.{css,js,mjs}"))
+  return src(projectPath(PATH_CONFIG.dest, "**/**.{css,js,mjs,map}"))
     .pipe(revReplace({ manifest: manifest }))
     .pipe(dest(projectPath(PATH_CONFIG.dest)));
 });
