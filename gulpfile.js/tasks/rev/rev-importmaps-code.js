de5728cb426f48cb174b87cad6d1291365337b6a
Aleš Roubíček <ales@roubicek.name>
2022-06-27 13:13:50 +0200

    Try to generate rev manifest and importmap in one pass

--- gulpfile.js/tasks/rev/rev-importmaps-code.js
+++ gulpfile.js/tasks/rev/rev-importmaps-code.js
@@ -1,19 +1,27 @@
 const { task, src, dest } = require("gulp");
 const rev = require("../../packages/gulp-rev");
 const revdel = require("gulp-rev-delete-original");
+const gulpMerge = require("gulp-merge");
 const projectPath = require("../../lib/projectPath");
 
 // 3) Rev and compress CSS and JS files (this is done after assets, so that if a
 //    referenced asset hash changes, the parent hash will change as well
-task("rev-importmaps-code", () =>
-  src([projectPath(PATH_CONFIG.dest, "**/*.{js,mjs}")])
+task("rev-importmaps-code", () => {
+  const revcode = src([projectPath(PATH_CONFIG.dest, "**/*.{js,mjs}")])
     .pipe(rev())
     .pipe(dest(projectPath(PATH_CONFIG.dest)))
-    .pipe(revdel())
-    .pipe(
+    .pipe(revdel());
+  return gulpMerge(
+    revcode.pipe(
       rev.importmap(projectPath(PATH_CONFIG.dest, "import-map.importmap"), {
         merge: true,
       })
+    ),
+    revcode.pipe(
+      rev.manifest(projectPath(PATH_CONFIG.dest, "rev-manifest.json"), {
+        merge: true,
+      })
     )
-    .pipe(dest("."))
+  ).pipe(dest("."));
+  }
 );
