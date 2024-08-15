7ff411884db05075e4b30a04f2edd48513276f4f
Aleš Roubíček <ales@roubicek.name>
2024-08-15 17:07:58 +0200

    propagate gulp exit code from blendid bin script

--- bin/blendid.js
+++ bin/blendid.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node
 const path = require("path");
+const childProcess = require("child_process");
 
 const blendidEntryDir = path.resolve(__dirname, "../gulpfile.js/index.mjs");
 const gulpModulePath = path.dirname(require.resolve("gulp"));
 const gulpBinaryFile = path.join(gulpModulePath, "/bin/gulp");
 
-require("child_process").fork(gulpBinaryFile, [
+const gulp = childProcess.fork(gulpBinaryFile, [
   "--cwd",
   process.cwd(),
   "--gulpfile",
   blendidEntryDir,
   ...process.argv.slice(2)
 ]);
+
+gulp.on("exit", (code) => {
+  process.exit(code);
+});
