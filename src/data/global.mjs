7ba3181459d5f90f04bc1edbd3c9915916b67d65
Aleš Roubíček <ales@roubicek.name>
2025-01-16 14:05:49 +0100

    Move generator info into meta data

--- src/data/global.mjs
+++ src/data/global.mjs
@@ -1,8 +1,9 @@
 export default {
   meta: {
-    lang: "en",
     title: "Blendid!",
+    url: "https://www.example.com/",
     description: "Awesome new static website. Such beauty. Such speed.",
-    url: "https://www.example.com/"
+    lang: "en",
+    generator: "@hckr_/blendid - static site generator and assets pipeline"
   }
 };
